08d6b3375de9c88fbacbf3de8b833c820391dcac
janthony25 <junanthony.business@gmail.com>
2024-02-03 19:04:11 +1300

    Add project files.

---
+++ OOP-Array-Of-Objects/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace MyFirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car[] garage =
+            {
+                new Car("Mustang"),
+                new Car("BMW"),
+                new Car("Mercedes Benz")
+            };
+
+            foreach (Car car in garage)
+            {
+                Console.WriteLine(car.model);
+            }
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+
+    class Car
+    {
+        public string model;
+
+        public Car(string model)
+        {
+            this.model = model;
+        }
+    }
+}
